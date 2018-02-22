57082ecc963e81febec2adff8c30b9e5eeba3f90
Paulow98 <paulow98@hotmail.com>
2018-02-22 11:56:27 +0000

    Done everything

--- JogarTenis/Program.cs
+++ JogarTenis/Program.cs
@@ -10,6 +10,41 @@ namespace JogarTenis
     {
         static void Main(string[] args)
         {
+            bool humido = false, chuva = false, vento = false;
+            if (args.Length >= 1)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "humido":
+                            humido = true;
+                            break;
+                        case "chuva":
+                            chuva = true;
+                            break;
+                        case "vento":
+                            vento = true;
+                            break;
+                        default:
+                            Console.WriteLine("ERRO, as opções possiveis são: humido chuva vento");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                            break;
+
+                    }
+                }
+            }
+
+            if(!chuva && !vento || !vento && !humido)
+            {
+                Console.WriteLine("Vamos jogar tenis!");
+            }
+            else
+            {
+                Console.WriteLine("Nao ha jogo hoje");
+            }
+            Console.ReadKey();
         }
     }
 }
